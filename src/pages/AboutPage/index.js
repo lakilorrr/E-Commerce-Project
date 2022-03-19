0a450877ccbd8a1ae3e971cbe31394c6c437b92e
lakilorrr <nao518543045@163.com>
2022-03-20 00:18:53 +0800

    add about

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import {Wrapper} from './style'
+import PageHero from '../../components/PageHero'
+import aboutImg from '../../assets/hero-bcg.jpeg'
+import { Wrapper } from './style'
 
 const About = () => {
-  return (
-    <Wrapper>About</Wrapper>
-  )
+    return (
+        <main>
+            <PageHero title='about' />
+            <Wrapper className='page section section-center'>
+                <img src={aboutImg} alt='desk' />
+                <article>
+                    <div className='title'>
+                        <h2>Our story</h2>
+                        <div className='underline'></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem delectus tempore asperiores. At ex, vel tempore corrupti ipsam molestiae
+                        quibusdam fuga sequi deserunt quod harum alias placeat. Quidem, sequi numquam.
+                    </p>
+                </article>
+            </Wrapper>
+        </main>
+    )
 }
 export default About
