aa7de2c05b5e6cecfdd8262ddc86a507087380b3
lakilorrr <nao518543045@163.com>
2022-03-19 23:44:29 +0800

    add error page

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import {Wrapper} from './style'
+import { Link } from 'react-router-dom'
+import { Wrapper } from './style'
 
 const Error = () => {
-  return (
-    <Wrapper>Error</Wrapper>
-  )
+    return (
+      <Wrapper className='page-100'>
+        <section>
+            <h1>404</h1>
+            <h3>Sorry, the page you tried cannot be foune</h3>
+            <Link to='/' className='btn'>
+                back home
+          </Link>
+          </section>
+        </Wrapper>
+    )
 }
 export default Error
